ffc68202872a04d33108d7db8a5fee623cb28563
“Austin” <30918019+haywang@users.noreply.github.com>
2025-02-21 10:56:49 +0800

    add tailwind css

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
