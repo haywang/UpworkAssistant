b02a7e4ccff4e7986feb899dbf8489ebe0b3212a
“Austin” <30918019+haywang@users.noreply.github.com>
2025-02-24 16:51:22 +0800

    get dom, return null

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
-<!-- <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script> -->
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+console.warn('pop page')
+
+console.warn(document.querySelector('.air3-badge-tagline'))
+// 获取第一个匹配的 DOM 元素
+const badgeElement = document.querySelector('.air3-badge-tagline strong');
+
+// 提取金额
+if (badgeElement) {
+  const amountText = badgeElement.textContent;
+  let amount = 0
+  if (amountText) {
+    amount = parseInt(amountText.replace(/[^0-9]/g, ''), 10);
+  }
+
+  console.log('提取的金额:', amount);
+} else {
+  console.log('未找到匹配的元素');
+}
+</script>
 
 <template>
   <!-- <div>
