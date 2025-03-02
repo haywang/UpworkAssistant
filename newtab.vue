400e153274349cb616f7eff502e85451ea30b3c2
“Austin” <30918019+haywang@users.noreply.github.com>
2025-03-02 22:26:17 +0800

    add pages

---
+++ newtab.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="{ display: 'flex', flexDirection: 'column', padding: 16 }">
+    <h1>
+      Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+    </h1>
+    <input />
+  </div>
+</template>
---
+++ options.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>
+      Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+    </h1>
+    <h2>This is the Option UI page!</h2>
+    <input />
+  </div>
+</template>
