087830140b87f5abf20c3b383b90da1d22cbd222
“Austin” <30918019+haywang@users.noreply.github.com>
2025-03-04 13:48:23 +0800

    add tabs page

---
+++ tabs/delta-flyer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    :class="{
+      display: 'flex',
+      flexDirection: 'column',
+      padding: 16
+    }">
+    <h2>Delta Flyer Tab</h2>
+
+    <p>This tab is only available on the Delta Flyer page.</p>
+  </div>
+</template>
