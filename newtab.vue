9a78d3f6343f21f7b3b3e29aa3a3a389d85f227e
“Austin” <30918019+haywang@users.noreply.github.com>
2025-03-05 08:52:48 +0800

    remove new tab page

--- newtab.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div :class="{ display: 'flex', flexDirection: 'column', padding: 16 }">
-    <h1>
-      Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-    </h1>
-    <input />
-  </div>
-</template>
